270a99fd78af4e2aa3b8869276cbe8f36c54e642
LukeWCS <luke@wcsaga.org>
2023-09-09 19:35:33 +0200

    1.1.2

--- filecheck.php
+++ filecheck.php
@@ -15,8 +15,9 @@
 /*
 * Initialization
 */
-define('EOL'		, "\n");
-define('IS_BROWSER'	, $_SERVER['HTTP_USER_AGENT'] ?? '' != '');
+define('EOL'			, "\n");
+define('VALID_CHARS'	, 'a-zA-Z0-9\/\-_.');
+define('IS_BROWSER'		, $_SERVER['HTTP_USER_AGENT'] ?? '' != '');
 
 $debug_mode				= false;
 
@@ -30,7 +31,7 @@ $ignore_file			= 'filecheck_ignore.txt';
 $exceptions_file		= 'filecheck_exceptions.txt';
 $constants_file			= 'includes/constants.php';
 
-$ver					= '1.1.1';
+$ver					= '1.1.2';
 $title					= "phpBB File Check v{$ver}";
 $unknown				= '{unknown}';
 $output					= html_start();
@@ -57,18 +58,30 @@ $output .= str_repeat('=', strlen($title)) . EOL . EOL;
 */
 if (file_exists($root_path . $constants_file))
 {
-	preg_match('/\'PHPBB_VERSION\'.*?\'([0-9]+\.[0-9]+\.[0-9]+)\'/', file_get_contents($root_path . $constants_file), $matches);
-	$PHPBB_VERSION = $matches[1] ?? null;
-	if ($PHPBB_VERSION !== null && !file_exists($root_path . $checksum_file))
+	$constants_content = @file_get_contents($root_path . $constants_file);
+	if ($constants_content !== false)
 	{
-		$checksum_file			= $checksum_file_name . '_' . $PHPBB_VERSION . $checksum_file_suffix;
-		$checksum_diff_file		= $checksum_file_name . '_' . $PHPBB_VERSION . '_diff' . $checksum_file_suffix;
-		$checksum_select_mode	= 'AUTO';
+		preg_match('/\'PHPBB_VERSION\'\s*,\s*\'([0-9]+\.[0-9]+\.[0-9]+)\'/', $constants_content, $matches);
+		$PHPBB_VERSION = $matches[1] ?? null;
+		if ($PHPBB_VERSION !== null && !file_exists($root_path . $checksum_file))
+		{
+			$checksum_file			= $checksum_file_name . '_' . $PHPBB_VERSION . $checksum_file_suffix;
+			$checksum_diff_file		= $checksum_file_name . '_' . $PHPBB_VERSION . '_diff' . $checksum_file_suffix;
+			$checksum_select_mode	= 'AUTO';
+		}
+		if ($PHPBB_VERSION === null)
+		{
+			$constants_notice = "NOTICE: phpBB version could not be determined from [{$constants_file}]" . EOL;
+		}
+	}
+	else
+	{
+		$constants_notice = "NOTICE: phpBB file [{$constants_file}] could not be read" . EOL;
 	}
 }
-else if (!file_exists($root_path . $checksum_file))
+else
 {
-	$output .= "NOTICE: phpBB file [{$constants_file}] not found" . EOL;
+	$constants_notice = "NOTICE: phpBB file [{$constants_file}] not found" . EOL;
 }
 
 /*
@@ -85,6 +98,7 @@ if (file_exists($root_path . $checksum_file))
 }
 else
 {
+	$output .= $constants_notice ?? '';
 	terminate("checksum file [{$checksum_file}] not found");
 }
 
@@ -93,8 +107,8 @@ if (file_exists($root_path . $checksum_diff_file))
 	load_checksum_file($root_path . $checksum_diff_file, $checksums_diff_name, $checksums_diff_ver, 2, $hash_list);
 }
 
-$count_checksums	= count($hash_list);
-$counter_len		= strlen($count_checksums);
+$count_checksums		= count($hash_list);
+$checksums_count_len	= strlen($count_checksums);
 
 /*
 * Load and check the external ignore list
@@ -103,7 +117,7 @@ if (file_exists($root_path . $ignore_file))
 {
 	$import_list	= @file($root_path . $ignore_file, FILE_IGNORE_NEW_LINES);
 	$line_num		= 0;
-	$regex_errors	= '';
+	$error_messages	= '';
 	if ($import_list !== false)
 	{
 		foreach ($import_list as $row)
@@ -111,13 +125,13 @@ if (file_exists($root_path . $ignore_file))
 			$line_num++;
 			if (@preg_match($row, '') === false)
 			{
-				$regex_errors .= sprintf('line %1$u: invalid RegEx "%2$s"', $line_num, $row) . EOL;
+				$error_messages .= "line {$line_num}: [{$row}] contains invalid RegEx" . EOL;
 			}
 		}
-		if ($regex_errors != '')
+		if ($error_messages != '')
 		{
-			add_list_lines($regex_errors);
-			terminate("ignore list [{$ignore_file}] has the following errors:" . EOL . $regex_errors);
+			add_list_lines($error_messages);
+			terminate("ignore list [{$ignore_file}] has the following issues:" . EOL . $error_messages);
 		}
 		$ignore_list = array_merge($ignore_list, $import_list);
 	}
@@ -128,13 +142,28 @@ if (file_exists($root_path . $ignore_file))
 }
 
 /*
-* Load the external exception list
+* Load and check the external exception list
 */
 if (file_exists($root_path . $exceptions_file))
 {
 	$import_list = @file($root_path . $exceptions_file, FILE_IGNORE_NEW_LINES);
+	$line_num		= 0;
+	$error_messages	= '';
 	if ($import_list !== false)
 	{
+		foreach ($import_list as $row)
+		{
+			$line_num++;
+			if (preg_match('/[^' . VALID_CHARS . ']/', $row))
+			{
+				$error_messages .= "line {$line_num}: [{$row}] contains invalid characters" . EOL;
+			}
+		}
+		if ($error_messages != '')
+		{
+			add_list_lines($error_messages);
+			terminate("exception list [{$exceptions_file}] has the following issues:" . EOL . $error_messages);
+		}
 		$exceptions_list = array_merge($exceptions_list, $import_list);
 	}
 	else
@@ -169,26 +198,22 @@ $count_warning		= 0;
 $count_notice		= 0;
 $count_ignored		= 0;
 $count_exceptions	= 0;
-$count_excluded		= 0;
 $count_checked		= 0;
-$index_results		= -1;
 $results_list		= [];
-foreach ($hash_list as $file => $data)
+foreach ($hash_list as $file => $hash_data)
 {
 	$is_ignored = is_ignored($file, $ignore_list);
 	if ($is_ignored || is_exception($file, $exceptions_list, $root_path))
 	{
-		$count_excluded++;
 		if ($debug_mode)
 		{
 			if ($is_ignored)
 			{
 				$count_ignored++;
-				$index_results++;
-				$results_list[$index_results] = [
+				$results_list[] = [
 					'path'			=> dirname($file),
-					'hash_file_id'	=> $hash_list[$file]['hash_file_id'],
-					'hash_line_num'	=> $hash_list[$file]['hashes'][0]['line_num'],
+					'hash_file_id'	=> $hash_data[0]['hash_file_id'],
+					'hash_line_num'	=> $hash_data[0]['hash_line_num'],
 					'msg_type'		=> '- IGNORED',
 					'file'			=> $file,
 					'msg'			=> '',
@@ -197,11 +222,10 @@ foreach ($hash_list as $file => $data)
 			else
 			{
 				$count_exceptions++;
-				$index_results++;
-				$results_list[$index_results] = [
+				$results_list[] = [
 					'path'			=> dirname($file),
-					'hash_file_id'	=> $hash_list[$file]['hash_file_id'],
-					'hash_line_num'	=> $hash_list[$file]['hashes'][0]['line_num'],
+					'hash_file_id'	=> $hash_data[0]['hash_file_id'],
+					'hash_line_num'	=> $hash_data[0]['hash_line_num'],
 					'msg_type'		=> '- EXCEPTION',
 					'file'			=> $file,
 					'msg'			=> '',
@@ -212,99 +236,92 @@ foreach ($hash_list as $file => $data)
 	}
 
 	$count_checked++;
-	if (file_exists($root_path . $file))
+	if (!file_exists($root_path . $file))
 	{
-		switch ($file)
-		{
-			case 'config.php':
-				$filesize = @filesize($file);
-				if ($filesize === 0)
-				{
-					$count_warning++;
-					$index_results++;
-					$results_list[$index_results] = [
-						'path'			=> dirname($file),
-						'hash_file_id'	=> $hash_list[$file]['hash_file_id'],
-						'hash_line_num'	=> $hash_list[$file]['hashes'][0]['line_num'],
-						'msg_type'		=> '! WARNING',
-						'file'			=> $file,
-						'msg'			=> 'has 0 bytes',
-					];
-				}
-				else if ($filesize === false)
-				{
-					$count_error++;
-					$index_results++;
-					$results_list[$index_results] = [
-						'path'			=> dirname($file),
-						'hash_file_id'	=> $hash_list[$file]['hash_file_id'],
-						'hash_line_num'	=> $hash_list[$file]['hashes'][0]['line_num'],
-						'msg_type'		=> '~ ERROR',
-						'file'			=> $file,
-						'msg'			=> 'file size could not be determined',
-					];
-				}
-				continue 2;
-		}
+		$count_missing++;
+		$results_list[] = [
+			'path'			=> dirname($file),
+			'hash_file_id'	=> $hash_data[0]['hash_file_id'],
+			'hash_line_num'	=> $hash_data[0]['hash_line_num'],
+			'msg_type'		=> '! MISSING',
+			'file'			=> $file,
+			'msg'			=> '',
+		];
+		continue;
+	}
 
-		$hash_calc = @md5_file($root_path . $file);
-		if ($hash_calc !== false)
-		{
-			if ($hash_list[$file]['hashes'][0]['hash'] != $hash_calc)
+	switch ($file)
+	{
+		case 'config.php':
+			$filesize = @filesize($file);
+			if ($filesize === 0)
 			{
-				if (($hash_list[$file]['hashes'][1]['hash'] ?? '') == $hash_calc)
-				{
-					$count_notice++;
-					$index_results++;
-					$results_list[$index_results] = [
-						'path'			=> dirname($file),
-						'hash_file_id'	=> 2,
-						'hash_line_num'	=> $hash_list[$file]['hashes'][1]['line_num'],
-						'msg_type'		=> '  NOTICE',
-						'file'			=> $file,
-						'msg'			=> 'has the ' . $checksums_diff_name . ' hash',
-					];
-				}
-				else
-				{
-					$count_changed++;
-					$index_results++;
-					$results_list[$index_results] = [
-						'path'			=> dirname($file),
-						'hash_file_id'	=> $hash_list[$file]['hash_file_id'],
-						'hash_line_num'	=> $hash_list[$file]['hashes'][0]['line_num'],
-						'msg_type'		=> '* CHANGED',
-						'file'			=> $file,
-						'msg'			=> '(hash: ' . $hash_calc . ')',
-					];
-				}
+				$count_warning++;
+				$results_list[] = [
+					'path'			=> dirname($file),
+					'hash_file_id'	=> $hash_data[0]['hash_file_id'],
+					'hash_line_num'	=> $hash_data[0]['hash_line_num'],
+					'msg_type'		=> '! WARNING',
+					'file'			=> $file,
+					'msg'			=> 'has 0 bytes',
+				];
 			}
-		}
-		else
+			else if ($filesize === false)
+			{
+				$count_error++;
+				$results_list[] = [
+					'path'			=> dirname($file),
+					'hash_file_id'	=> $hash_data[0]['hash_file_id'],
+					'hash_line_num'	=> $hash_data[0]['hash_line_num'],
+					'msg_type'		=> '~ ERROR',
+					'file'			=> $file,
+					'msg'			=> 'file size could not be determined',
+				];
+			}
+			continue 2;
+	}
+
+	$hash_calc = @md5_file($root_path . $file);
+	if ($hash_calc !== false)
+	{
+		if ($hash_data[0]['hash'] != $hash_calc)
 		{
-			$count_error++;
-			$index_results++;
-			$results_list[$index_results] = [
-				'path'			=> dirname($file),
-				'hash_file_id'	=> $hash_list[$file]['hash_file_id'],
-				'hash_line_num'	=> $hash_list[$file]['hashes'][0]['line_num'],
-				'msg_type'		=> '~ ERROR',
-				'file'			=> $file,
-				'msg'			=> 'MD5 hash could not be calculated',
-			];
+			if (($hash_data[1]['hash'] ?? '') == $hash_calc)
+			{
+				$count_notice++;
+				$results_list[] = [
+					'path'			=> dirname($file),
+					'hash_file_id'	=> $hash_data[1]['hash_file_id'],
+					'hash_line_num'	=> $hash_data[1]['hash_line_num'],
+					'msg_type'		=> '  NOTICE',
+					'file'			=> $file,
+					'msg'			=> 'has the ' . $checksums_diff_name . ' hash',
+				];
+			}
+			else
+			{
+				$count_changed++;
+				$results_list[] = [
+					'path'			=> dirname($file),
+					'hash_file_id'	=> $hash_data[0]['hash_file_id'],
+					'hash_line_num'	=> $hash_data[0]['hash_line_num'],
+					'msg_type'		=> '* CHANGED',
+					'file'			=> $file,
+					'msg'			=> '(hash: ' . $hash_calc . ')',
+				];
+			}
 		}
 	}
 	else
 	{
-		$count_missing++;
-		$index_results++;
-		$results_list[$index_results] = [
+		$count_error++;
+		$results_list[] = [
 			'path'			=> dirname($file),
-			'hash_file_id'	=> $hash_list[$file]['hash_file_id'],
-			'hash_line_num'	=> $hash_list[$file]['hashes'][0]['line_num'],
-			'msg_type'		=> '! MISSING',
+			'hash_file_id'	=> $hash_data[0]['hash_file_id'],
+			'hash_line_num'	=> $hash_data[0]['hash_line_num'],
+			'msg_type'		=> '~ ERROR',
 			'file'			=> $file,
-			'msg'			=> '',
+			'msg'			=> 'MD5 hash could not be calculated',
 		];
 	}
 }
@@ -318,11 +335,11 @@ if (count($results_list) > 0)
 		return [$a['path'], $a['file']] <=> [$b['path'], $b['file']];
 	});
 
-	$column_msg_type = array_column($results_list, 'msg_type');
-	$msg_type_len = max(array_map('strlen', $column_msg_type));
+	$line_num_len = column_max_len($results_list, 'hash_line_num');
+	$msg_type_len = column_max_len($results_list, 'msg_type');
 	foreach ($results_list as $row)
 	{
-		$output .= sprintf('{%1$u:%2$ ' . $counter_len . 'u} %3$ -' . $msg_type_len . 's: [%4$s]%5$s',
+		$output .= sprintf('{%1$u:%2$ ' . $line_num_len . 'u} %3$ -' . $msg_type_len . 's: [%4$s]%5$s',
 			/* 1 */ $row['hash_file_id'],
 			/* 2 */ $row['hash_line_num'],
 			/* 3 */ $row['msg_type'],
@@ -344,23 +361,23 @@ add_list_lines($output);
 $output .= EOL;
 if ($debug_mode)
 {
-	$output .= sprintf('Ignored      : % ' . $counter_len . 'u', $count_ignored) . EOL;
-	$output .= sprintf('Exceptions   : % ' . $counter_len . 'u', $count_exceptions) . EOL;
+	$output .= sprintf('Ignored      : % ' . $checksums_count_len . 'u', $count_ignored) . EOL;
+	$output .= sprintf('Exceptions   : % ' . $checksums_count_len . 'u', $count_exceptions) . EOL;
 }
-$output .= sprintf('Checked files: % ' . $counter_len . 'u', $count_checked) . EOL;
-$output .= sprintf('Missing files: % ' . $counter_len . 'u', $count_missing) . EOL;
+$output .= sprintf('Checked files: % ' . $checksums_count_len . 'u', $count_checked) . EOL;
+$output .= sprintf('Missing files: % ' . $checksums_count_len . 'u', $count_missing) . EOL;
 if ($count_warning || $debug_mode)
 {
-	$output .= sprintf('Warnings     : % ' . $counter_len . 'u', $count_warning) . EOL;
+	$output .= sprintf('Warnings     : % ' . $checksums_count_len . 'u', $count_warning) . EOL;
 }
-$output .= sprintf('Changed files: % ' . $counter_len . 'u', $count_changed) . EOL;
+$output .= sprintf('Changed files: % ' . $checksums_count_len . 'u', $count_changed) . EOL;
 if ($count_notice || $debug_mode)
 {
-	$output .= sprintf('Notices      : % ' . $counter_len . 'u', $count_notice) . EOL;
+	$output .= sprintf('Notices      : % ' . $checksums_count_len . 'u', $count_notice) . EOL;
 }
 if ($count_error || $debug_mode)
 {
-	$output .= sprintf('Errors       : % ' . $counter_len . 'u', $count_error) . EOL;
+	$output .= sprintf('Errors       : % ' . $checksums_count_len . 'u', $count_error) . EOL;
 }
 
 $output .= EOL;
@@ -456,68 +473,68 @@ function add_list_lines(string &$text): void
 function load_checksum_file(string $checksum_file, string &$checksums_name, string &$checksums_ver, int $hash_file_id, array &$hash_list): void
 {
 	$checksums = @file($checksum_file, FILE_IGNORE_NEW_LINES);
-	$md5_errors = '';
-	if ($checksums !== false)
+	$checksum_basename = basename($checksum_file);
+	if ($checksums === false)
 	{
-		preg_match('/^(.*?):([0-9]+\.[0-9]+\.[0-9]+)/', end($checksums), $matches);
-		if (is_array($matches) && count($matches) == 3)
-		{
-			array_pop($checksums);
-			$checksums_name	= $matches[1];
-			$checksums_ver	= $matches[2];
-		}
-		else
-		{
-			terminate("checksum file [{$checksum_file}] does not have a valid version");
-		}
+		terminate("checksum file [{$checksum_basename}] could not be read");
+	}
 
-		$line_num = 0;
-		foreach ($checksums as $row)
-		{
-			$line_num++;
-			preg_match('/^([0-9a-f]{32}) \*([^\\\\:*?"<>|]+)$/', $row, $matches);
-			if (is_array($matches) && count($matches) == 3)
-			{
-				$hash = $matches[1];
-				$file = $matches[2];
-
-				if (isset($hash_list[$file]))
-				{
-					$hash_list[$file]['hashes'] += [
-						1	=> [
-							'line_num'	=> $line_num,
-							'hash'		=> $hash,
-						]
-					];
-				}
-				else
-				{
-					$hash_list += [
-						$file	=> [
-							'hash_file_id'	=> $hash_file_id,
-							'hashes'		=> [
-								0	=> [
-									'line_num'	=> $line_num,
-									'hash'		=> $hash,
-								],
-							]
-						]
-					];
-				}
-			}
-			else
-			{
-				$md5_errors .= sprintf('line %1$u: invalid hash row: "%2$s"', $line_num, $row) . EOL;
-			}
-		}
-		if ($md5_errors != '')
-		{
-			add_list_lines($md5_errors);
-			terminate("checksum file [{$checksum_file}] has the following errors:" . EOL . $md5_errors);
-		}
+	preg_match('/^(.*?):([0-9]+\.[0-9]+\.[0-9]+)/', end($checksums), $matches);
+	if (is_array($matches) && count($matches) == 3)
+	{
+		array_pop($checksums);
+		$checksums_name	= $matches[1];
+		$checksums_ver	= $matches[2];
 	}
 	else
 	{
-		terminate("checksum file [{$checksum_file}] could not be read");
+		terminate("checksum file [{$checksum_basename}] does not have a valid version");
 	}
+
+	$line_num		= 0;
+	$error_messages	= '';
+	foreach ($checksums as $row)
+	{
+		$line_num++;
+		preg_match('/^([0-9a-f]{32}) \*([' . VALID_CHARS . ']+)$/', $row, $matches);
+		if (is_array($matches) && count($matches) == 3)
+		{
+			$hash = $matches[1];
+			$file = $matches[2];
+
+			if (isset($hash_list[$file]))
+			{
+				$hash_list[$file][] = [
+					'hash_file_id'	=> $hash_file_id,
+					'hash_line_num'	=> $line_num,
+					'hash'			=> $hash,
+				];
+			}
+			else
+			{
+				$hash_list[$file] = [
+					[
+						'hash_file_id'	=> $hash_file_id,
+						'hash_line_num'	=> $line_num,
+						'hash'			=> $hash,
+					]
+				];
+			}
+		}
+		else
+		{
+			$error_messages .= "line {$line_num}: [{$row}] invalid row" . EOL;
+		}
+	}
+	if ($error_messages != '')
+	{
+		add_list_lines($error_messages);
+		terminate("checksum file [{$checksum_basename}] has the following issues:" . EOL . $error_messages);
+	}
+}
+
+function column_max_len(array &$array, string $column_name): int
+{
+	$column = array_column($array, $column_name);
+	return max(array_map('strlen', $column));
 }
--- phpbb-file-check_changelog_de.md
+++ phpbb-file-check_changelog_de.md
@@ -1,5 +1,16 @@
+### 1.1.2
+(2023-09-09)
+
+* Fehlerbehandlung für die phpBB Datei `includes/constants.php` erweitert:
+  * Kann die Datei nicht erfolgreich gelesen werden, wird das explizit als Hinweis gemeldet.
+  * Kann aus der Datei die phpBB Version nicht ermittelt werden, wird das explizit als Hinweis gemeldet.
+* Dateinamen in Prüfsummen-Dateien werden strenger geprüft; die erlaubten Zeichen sind jetzt explizit vorgegeben. Bisher wurde nur auf einige wenige unerlaubte Zeichen geprüft.
+* Die externe Ausnahme-Liste wird jetzt ebenfalls auf erlaubte Zeichen geprüft. Dafür wird mit demselben Muster geprüft, wie bei den Prüfsummen-Dateien.
+* Kleine Änderungen.
+* Code Optimierung.
+
 ### 1.1.1
-(25.8.2023)
+(2023-08-25)
 
 * Fix: Wenn die Ignorieren-Liste `filecheck_ignore.txt` vorhanden war und diese mehrere ungültige RegEx Einträge enthielt, wurde nur der letzte gemeldet.
 * Fix: Bei Ausführung im Browser wurden die abschliessenden HTML Tags nicht generiert, wenn mit einer Fehlermeldung abgebrochen wurde.
@@ -13,7 +24,7 @@
 * Haupt-Code kommentiert.
 
 ### 1.1.0 
-(20.7.2023)
+(2023-07-20)
 
 * Fix: Bei Verwendung in der Shell wurde bei einer Fehlermeldung bezüglich fehlender Prüfsummen-Datei der HTML Footer generiert.
 * Um die Problematik der Abweichungen bei phpBB.de und phpBB.com Paketen in den Griff zu bekommen, mussten einige Änderungen vorgenommen werden. Die Basis für FC ist nicht mehr das deutsche Komplettpaket, sondern das phpBB.com Paket. Um alle zusätzlichen und abweichenden Dateien des deutschen Komplettpakets berücksichtigen zu können, wird nun eine zweite Prüfsummen-Datei unterstützt, in der alle Abweichungen gegenüber dem phpBB.com Paket enthalten sind. FC kombiniert dann während der Ausführung beide Prüfsummen-Dateien. Somit können nun auch die abweichenden Dateien aussagekräftig geprüft werden, die bisher je nach Installationspaket möglicherweise falsch als `* CHANGED` gemeldet wurden.
@@ -31,28 +42,25 @@
 * Die externe Ignorieren-Liste wird jetzt, sofern vorhanden, auf gültiges RegEx geprüft. Ist ungültiges RegEx vorhanden, wird mit Fehlermeldung abgebrochen.
 * Fehlerbehandlung auch an anderen Stellen erweitert und Fehlertoleranz bezüglich FC Dateien deutlich reduziert. Das betrifft besonders die Prüfsummen-Dateien.
 * Wenn FC bei Erkennung eines User-Agent-Strings den Bericht im HTML Format ausliefert, ist jetzt ein Meta Tag enthalten, der Suchmaschinen Indizierung verhindern soll. Das kann hilfreich sein, wenn vergessen wurde FC nach Benutzung zu löschen. Das funktioniert natürlich nur bei Crawlern, die sich auch an die Regeln halten. Trotzdem sollte FC auch weiterhin vom FTP Server gelöscht werden, wenn es nicht mehr benötigt wird.
-* Code Optimierung
-* Prüfsummen-Paket komplett neu erstellt. Pro phpBB Version gibt es jetzt 2 Prüfsummen-Dateien; eine vollständige für das phpBB.com Paket und eine für das phpBB.de Komplettpaket, welche nur die Abweichungen zum phpBB.com Paket enthält. Die alten Prüfsummen-Dateien werden nicht mehr akzeptiert.
-* Erklärung der Meldungen angepasst.
-* Anleitung angepasst. Ausserdem gelbe Info-Box entfernt, da das Installationspaket (original phpBB.com Paket oder phpBB.de Komplettpaket) nicht länger relevant ist.
+* Code Optimierung.
 
 ### 1.0.0
-(12.7.2023)
+(2023-07-12)
 
 * Erste öffentliche Version.
 * Ausser Versionsänderung keinen Unterschied zu 0.5.1.
 
 ### 0.5.1
-(11.7.2023)
+(2023-07-11)
 
 * Die Zeile mit der Variable für den Debug-Modus ist kein Kommentar mehr, sondern eine aktive Codezeile. Die Variable ist per Standard mit `false` belegt und kann bei Bedarf auf `true` gesetzt werden.
-* Nachfolgende Ordner wurden zu den Ausnahmen hinzugefügt:
+* Nachfolgende Ordner wurden zu den internen Ausnahmen hinzugefügt:
   * `docs/`
 * Auch Fehlermeldungen die einen Abbruch von FC zur Folge haben, zum Beispiel bei einer fehlenden Prüfsummen-Datei, sind jetzt in ein HTML Gerüst eingebettet, sofern FC im Browser ausgeführt wird.
 * Ausserdem wird jetzt zumindest der FC Titel immer ausgegeben, also auch bei Abbruch.
 
 ### 0.5.0
-(10.7.2023)
+(2023-07-10)
 
 * Da ich festgestellt habe, dass in den phpBB Paketen einige überflüssige Dateien vorhanden sind, mussten weitere Einträge zur Ignorieren-Liste hinzugefügt werden. Bei den besagten Dateien handelt es sich um git Dateien, die in einem Distributionspaket für Endbenutzer eigentlich nichts zu suchen haben. Damit diese Dateien nicht umständlich einzeln hinzugefügt werden müssen, was auch nicht zukunftssicher wäre, wurde die Ignorieren-Funktion von einem simplen Textvergleich auf RegEx umgestellt. Dadurch ist die Ignorieren-Funktion nun erheblich flexibler verwendbar.
 * Um genau verfolgen zu können, welche Dateien von FC ignoriert und welche per Ausnahme ausgeschlossen wurden, kann man jetzt bei Bedarf den neuen Debug-Modus aktivieren. Dazu muss im FC Skript einfach die Zeile `$debug_mode = true` aktiviert werden. Dadurch werden folgende zusätzliche Anzeigen generiert:
@@ -64,7 +72,7 @@
 * Kleine Code Verbesserungen.
 
 ### 0.4.0
-(8.7.2023)
+(2023-07-08)
 
 * Bei Dateizugriffen werden statt relative Pfade jetzt absolute Pfade verwendet. In Meldungen werden nach wie vor relative Pfade angezeigt.
 * Die Einleitung "Please wait, we are checking x files..." war nicht korrekt, da es von den gültigen MD5 Einträgen sowie von den Listen der Ignorierten und Ausnahmen abhängt, wieviele Dateien tatsächlich geprüft werden. Darum Einleitung geändert in "Please wait, x MD5 entries are being processed...".
@@ -75,23 +83,23 @@
 * Kleine Code Verbesserungen.
 
 ### 0.3.0
-(6.7.2023)
+(2023-07-06)
 
 * Ab dieser Version kann FC selbständig die richtige MD5 laden, sofern vorhanden. Dazu muss die passende `filecheck_x.y.z.md5` vorhanden sein. Dieser Modus wird dann durch den Zusatz "(auto)" signalisiert. 
 * Der automatische Modus kann jederzeit mittels `filecheck.md5` übersteuert werden. In diesem Fall wird der manuelle Modus aktiviert und alle anderen `filecheck_x.y.z.md5` Dateien werden ignoriert. Dieser Modus wird dann durch den Zusatz "(manually)" signalisiert. Das kann in seltenen Fällen notwendig sein, wenn FC die phpBB Version nicht ermitteln kann.
 * FC und MD5 Dateien sind jetzt in getrennten Archiven organisiert, so wie es später dann auch bei Veröffentlichung auf phpBB.de der Fall sein wird.
 * Bei Verwendung im Browser wurde bisher noch der alte Seitentitel "phpBB CheckFiles" verwendet. Jetzt wird der korrekte Titel angezeigt.
 * Code Optimierung.
-* Kleinkram.
+* Kleine Änderungen.
 * Liste der Eigenschaften und Anleitung aktualisiert.
 
 ### 0.2.1
-(3.7.2023)
+(2023-07-03)
 
 * Fix: Bei PHP <8.0 wurde "Uncaught Error: Call to undefined function str_starts_with()" erzeugt. [Meldung von Scanialady]
 
 ### 0.2.0
-(3.7.2023)
+(2023-07-03)
 
 * Fix: Bei fehlender `constants.php` wurde "Warning: Undefined variable" erzeugt. Im Prototyp (0.0.x) war das bereits berücksichtigt, jedoch nicht mehr ab 0.1.0.
 * Umbenannt von "phpBB CheckFiles" zu "phpBB File Check". Das betrifft auch die Dateinamen und die Anleitung.
@@ -99,9 +107,11 @@
 * Die Anzahl Zeichen für rechtsbündig ausgerichtete Zahlen ist nicht mehr auf 4 fixiert, sondern wird dynamisch anhand der Anzahl Zeilen in der Prüfsummen-Datei ermittelt.
 * Die Liste der Fehlermeldungen ist jetzt in 2 Bindestrich-Linien eingefasst.
 * Es gibt jetzt die Möglichkeit, Ignorierte und Ausnahmen in einer externen Datei definieren zu können, ohne das Skript ändern zu müssen: `filecheck_ignore.txt` und `filecheck_exceptions.txt`. Das ist eine der Voraussetzungen, damit das Skript auch mit dem original phpBB.com Paket sowie anderen (nationalen) Sprachpaketen verwendet werden kann, da dann jedes nationale Support Forum angepasste Regeln verwenden kann.
-* Nachfolgende Ordner wurden zu den Ausnahmen hinzugefügt:
+* Nachfolgende Ordner wurden zu den externen Ausnahmen hinzugefügt:
   * `ext/phpbb/viglink/language/de/`
   * `ext/phpbb/viglink/language/de_x_sie/`
+  * `language/de/`
+  * `language/de_x_sie/`
   * `styles/prosilver/theme/de/`
   * `styles/prosilver/theme/de_x_sie/`
 * Kleinere Code Verbesserungen.
@@ -109,14 +119,14 @@
 * Weiteren deaktivierten Code entfernt.
 
 ### 0.1.2
-(2.7.2023)
+(2023-07-02)
 
-* Für VigLink ebenfalls eine Ordner Ausnahme definiert. Ist die Ext vorhanden, wird sie auch vollständig geprüft. Ist sie nicht vorhanden, wird sie komplett ignoriert und auch keine MISSINGs mehr ausgegeben.
+* Für VigLink ebenfalls eine interne Ausnahme (`ext/phpbb/viglink/`) definiert. Ist die Ext vorhanden, wird sie auch vollständig geprüft. Ist sie nicht vorhanden, wird sie komplett ignoriert und auch keine MISSINGs mehr ausgegeben.
 * In der Zusammenfassung werden die Zahlen jetzt ebenfalls rechtsbündig ausgegeben, wie bei den Zeilennummern.
 * Restlichen deaktivierten Code entfernt.
-* Kleinkram.
+* Kleine Änderungen.
 
 ### 0.1.1
-(1.7.2023)
+(2023-07-01)
 
 * Erste interne Team Version.
